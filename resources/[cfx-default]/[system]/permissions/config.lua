05fb9b55fc5caf3296763f5c8a74b3fa6a740782
reinis <raymans121@gmail.com>
2025-12-02 05:29:19 +0200

    Update config.lua

--- resources/[cfx-default]/[system]/permissions/config.lua
+++ resources/[cfx-default]/[system]/permissions/config.lua
@@ -1,21 +1,19 @@
 Config = {
     Server_Name = "SERVER",
-    Discord_Link = 'https://discord.gg/',
+    Discord_Link = 'https://discord.gg/ZyJayZebYx',
     Website_Link = '',
     Allow_Refresh_Command = true, -- Allow usage of /refreshPerms command
     Refresh_Throttle = 600, -- 10 minute refresh throttle
 
-    Guild_ID = '', -- Set to the ID of your guild (or your Primary guild if using Multiguild)
+    Guild_ID = '1444121670867484885', -- Set to the ID of your guild (or your Primary guild if using Multiguild)
     Bot_Token = '',
     CacheDiscordRoles = true, -- true to cache player roles, false to make a new Discord Request every time
     CacheDiscordRolesTime = 60, -- if CacheDiscordRoles is true, how long to cache roles before clearing (in seconds)
     roleList = {
-        --{778070857964716033, "group.member"},
-        -- {1237810363177897986, "group.mod"}, -- Moderator
-        -- {1435305527436509345, "group.admin"}, -- Jr. Admin
-        -- {1237810420312707199, "group.admin"}, -- Admin
-        -- {1283410711699263528, "group.dev"}, -- Dev
-        -- {1435228380193755146, "group.owner"}, -- Līdzīpaš.
-        -- {1237810473315864636, "group.owner"}, -- Īpaš.
+        {778070857964716033, "group.member"},
+        {1444122083054325930, "group.mod"}, -- Moderator
+        {1444122123827281981, "group.admin"}, -- Admin
+        {1444122180924473384, "group.dev"}, -- Dev
+        {1444122210959622245, "group.owner"}, -- Īpaš.
     },
 }
