337d985614c23e2bb006eae8dbdd51467c51a8ec
reinis <raymans121@gmail.com>
2025-12-05 02:58:39 +0200

    blip

--- resources/[qbx]/blips_creator/language.lua
+++
@@ -1,3 +0,0 @@
-language = language or {}
-
-language['confirm_blip'] = "~y~%s~s~ shared '~o~%s~s~' blip\n~INPUT_FRONTEND_RDOWN~ to ~g~accept~s~\n~INPUT_FRONTEND_RRIGHT~ to ~r~deny~s~"
--- resources/[qbx]/blips_creator/sh_config.lua
+++
@@ -1,7 +0,0 @@
-config = config or {}
-
---[[
-    Allow the menu to admins only
-    (It's not recommended because non admin players will be able only to create local blips, that only them will see)
-]]
-config.adminsOnly = true
